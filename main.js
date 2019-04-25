ece5f64c8847caebac42961cd8f88caf8d2ebe2f
Marcel Konjata <rhonin33@gmail.com>
2019-04-25 13:49:18 +0200

    adding few comments into js file

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var mobileNavList = document.querySelector(".navigation-mobile-popup");
 var mobileNavLinks = document.querySelector(".navigation-mobile-popup")
   .children;
 //wasnt sure if the queryselector all returns html collection or node-list, I tried to avoid Array.from to secure at least higher compatibility
+// or doing another operation with for loop - thta would decrease performance and code would be less "DRY"
 
 console.log(mobileNavLinks);
 
@@ -40,7 +41,8 @@ window.addEventListener("scroll", function() {
     remove_class_on_scroll();
   }
 });
-
+//declaring and working with elements which I need for animations on scroll,
+//basically changing tranistion properties with adding extra class of element coming into viewport
 function animateInView() {
   const cards = document.querySelectorAll(".card");
   const banners = document.querySelectorAll(".banner");
